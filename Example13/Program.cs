d5c816bb0a618bce6470b4101f6ef7375be5a5b9
Den <no.den2018@yandex.ru>
2023-07-12 13:20:51 +0500

    Сделал 13 задание

---
+++ Example13/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры не
+Console.Write("Введите любой число: ");
+int theSecondFigure = int.Parse(Console.ReadLine());
+int number = theSecondFigure;
+if(theSecondFigure < 100)
+{
+    Console.WriteLine($"Число: {theSecondFigure} - не имеет третьей цифры");
+}
+if(theSecondFigure>=1000 )
+{
+while(theSecondFigure>=1000)
+{
+theSecondFigure /=10;
+}
+while(theSecondFigure > 9)
+{
+theSecondFigure%=10;
+}
+ Console.WriteLine($"Третья цифра {theSecondFigure} числа - {number}");
+}
+if(theSecondFigure >=100 && theSecondFigure <=999)
+{
+    theSecondFigure%=100;
+     theSecondFigure%=10;
+    Console.WriteLine($"Третья цифра {theSecondFigure} числа - {number}");
+}
